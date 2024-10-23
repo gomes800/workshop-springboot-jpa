e814a23848b497b73ef6a0a65605423e25461b12
barox <joaogomesleite800@gmail.com>
2024-10-23 15:20:05 -0300

    Exception handling - findById

--- src/main/java/com/gom/course/services/UserService.java
+++ src/main/java/com/gom/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.gom.course.services;
 
 import com.gom.course.entities.User;
 import com.gom.course.repositories.UserRepository;
+import com.gom.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findByID(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
